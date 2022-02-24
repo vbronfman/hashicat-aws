025a2fc49f9c241ad8004f9135cca09b6b696abb
Your Name <you@example.com>
2022-02-24 14:52:36 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "hashicat" {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
     Department = "devops"
-    Billable  = "true"
+    Billable  = true
   }
 }
 
